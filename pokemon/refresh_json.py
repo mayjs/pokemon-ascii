fe89aa285a1c419f102288d2b18fe3ac64c2f425
may <johannes@may-se.de>
2017-01-03 01:10:50 +0100

    Added edge detection

--- pokemon/refresh_json.py
+++ pokemon/refresh_json.py
@@ -4,18 +4,22 @@ import re
 from convert import handle_image_conversion
 
 os.chdir("database")
+# convert pokemon images to greyscale, remember to checkout the image directory before doing this
+# os.system("mogrify -charcoal 1 -white-threshold 75% -blur 8 -black-threshold 90% images/*.jpg")
 
 pkmns = None
 with open("pokemons.json") as f:
     pkmns = json.load(f)
 
-# for img in os.listdir("images"):
-#     if img.endswith(".jpg"):
-#         pid = img[:-4]  # drop .jpg
-#         ascii = handle_image_conversion("images" + os.sep + img, new_width=30).replace("#", " ")
-#         pkmns[pid]["ascii"] = ascii
+npkmns = {}
 
-with open("pokemons.json", "w") as f:
-    json.dump(pkmns, f)
+for img in os.listdir("images"):
+    if img.endswith(".jpg"):
+        pid = img[:-4]  # drop .jpg
+        ascii = handle_image_conversion("images" + os.sep + img, new_width=30).replace("@", " ")
+        npkmns[pkmns[pid]["name"]] = ascii
 
-print(pkmns["1"]["ascii"])
+with open("pokemons_new.json", "w") as f:
+    json.dump(npkmns, f)
+
+print(npkmns["Bulbasaur"])
